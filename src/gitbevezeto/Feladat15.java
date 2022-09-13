e0619b6ddf59b8a9eae10288b0c8a77cf64574b4
Bota laszlo Levente <botond.pfeffer@gmail.com>
2022-09-13 16:13:49 +0200

    Feladat 14 befejezése

--- src/gitbevezeto/Feladat15.java
+++ src/gitbevezeto/Feladat15.java
@@ -20,6 +20,43 @@ public class Feladat15 {
             System.out.println("\tf - Kilépni");
             System.out.print("Adja meg a menüpontot");
             menüpont = sc.nextLine().toLowerCase();
+            switch (menüpont){
+                case "a" :
+                    System.out.println(szöveg.toUpperCase());
+                    break;
+                case "b" :
+                    System.out.println(szöveg.toLowerCase());
+                    break;
+                case "c" :
+                    System.out.printf("A szöveg %d karakter\n");
+                    break;
+                case "d" :
+                    System.out.println("Kérem adjon meg egy másik szöveget");
+                    String szoveg2 = sc.nextLine();
+                    int osszehasonlitas = szöveg.compareTo(szoveg2);
+                    if (osszehasonlitas < 0){
+                        System.out.println("A második szöveg ban előrébb az ABC-ben");
+                    } else if (osszehasonlitas > 0){
+                        System.out.println("A második szöveg van előrébb");
+                    } else {
+                        System.out.println("A két szöveg megegyezik");
+                    }
+                    break;
+                case "e" :
+                    System.out.println("Kérem adja meg a kezdő karakterszámát amit ki szeretn eírni");
+                    int a = sc.nextInt();
+                    sc.nextLine();
+                    System.out.println("Kérem adja meg a karakterszámát amit ki szeretne írni");
+                    sc.nextLine();
+                    System.out.println(szöveg.substring(a - 1));
+                    break;
+                case "f" :
+                    System.out.println("Viszlát!");
+                    break;
+                default:
+                    System.out.println("Rossz lehetőséget adok meg: ");
+                    break;
+            }
         }
     }
 }
